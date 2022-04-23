b9f2da7390afb9a7abe2f0b32a54e988acebf322
Matthew Zhao <matthewhrzhao@gmail.com>
2022-04-23 17:03:21 -0500

    add assignment 4 notebook and script

---
+++ Psets/Assignment_4/Knn_regression.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rc('font', size=14)
+import seaborn as sns
+sns.set(style='white')
+sns.set(style='whitegrid', color_codes=True)
+from sklearn.neighbors import KNeighborsRegressor
+
+def Knn_visualize(x,y,k):
+    '''
+       Perform K-nn regression and visualize the results
+
+       Input:
+       x     : Regressor, dimension 100 * 1
+       y     : label, dimension 100 *1 
+       k     : Number of neighbors 
+
+       '''
+
+    plt.figure(figsize = (8,6))
+
+    plt.plot(x, y, 'o')
+    plt.plot(x, np.sin(x))
+    
+    
+##############################################################################
+### TODO: Realize k-nn in regression  task and train a model called nn.                                                             ###
+##############################################################################
+    # Perform k-NN regression
+    
+    nn = 
+    
+##############################################################################
+#                               END OF YOUR CODE                             #
+##############################################################################
+
+    # Visualization
+    plt.plot(x, nn.predict(x), 'green')
+    plt.title('Simulation example with {}-NN fit'.format(k))
+
+    plt.show()
+
